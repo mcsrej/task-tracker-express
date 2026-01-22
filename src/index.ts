16f8cbb87021cfd3d16525cfed73fe53e55b2de1
mcsrejer <mcsrej@mail.ru>
2026-01-22 16:35:01 +0300

    Add basic auth

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
 import express from 'express';
-import { PORT } from './config'
+import { ADMIN_LOGIN, ADMIN_PASSWORD, PORT } from './config'
 import { cardsRouter } from './routers/cards.router';
 import { createTables } from './database/create-tables';
+import basicAuth from 'express-basic-auth';
 
 async function run(){
     await createTables()
 
     const server = express();
     server.use(express.json());
+    server.use(basicAuth({
+        users: {[ADMIN_LOGIN]: ADMIN_PASSWORD},
+        challenge: true,
+    }));
 
     server.get('/', (request, response) => {
       response.send('ok');
